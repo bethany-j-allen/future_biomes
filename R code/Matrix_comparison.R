9975561758bc95194323957815bf54a4f0615d38
Bethany Allen <allenb@bs-mbpr540.d.ethz.ch>
2022-06-21 12:43:15 +0100

    Add megabiomes

--- R code/Matrix_comparison.R
+++ R code/Matrix_comparison.R
@@ -5,6 +5,7 @@ library(tidyverse)
 
 #Read in biome IDs
 biomes <- read.csv("data/cleaned/RCP6.csv")
+megabiomes <- read.csv("data/cleaned/RCP6_mega.csv")
 
 #List columns
 slices <- c("X2000.2019", "X2020.2039", "X2040.2059", "X2060.2079", "X2080.2099",
@@ -15,40 +16,59 @@ slices <- c("X2000.2019", "X2020.2039", "X2040.2059", "X2060.2079", "X2080.2099"
 
 #Count the biome changes between adjacent time slices
 change_counts <- c()
+mega_counts <- c()
 
 for (i in 1:(length(slices)-1)){
-  change_counts[i] <- length(which(biomes[,i] != biomes[,i+1]))
+  change_counts[i] <- length(which(biomes[,i+2] != biomes[,i+3]))
+  mega_counts[i] <- length(which(megabiomes[,i+2] != megabiomes[,i+3]))
 }
 
 #Add slice midpoints to plot against
 midpoints <- seq(from = 2020, to = 2480, by = 20)
-to_plot <- as.data.frame(cbind(midpoints, change_counts))
+to_plot_biomes <- as.data.frame(cbind(midpoints, change_counts))
+to_plot_mega <- as.data.frame(cbind(midpoints, mega_counts))
 
 #Plot
-ggplot(data = to_plot, aes(x = midpoints, y = change_counts)) +
+ggplot(data = to_plot_biomes, aes(x = midpoints, y = change_counts)) +
   geom_line() +
   xlab("Year") + ylab("Number of grid cell biome transitions") +
+  theme_classic()
+
+ggplot(data = to_plot_mega, aes(x = midpoints, y = mega_counts)) +
+  geom_line() +
+  xlab("Year") + ylab("Number of grid cell megabiome transitions") +
   theme_classic()
 
 
 #Split results into latitude bands
 high_lat <- filter(biomes, between(lat, 60, 90))
 high_lat <- rbind(high_lat, filter(biomes, between(lat, -90, -60)))
+high_lat_m <- filter(megabiomes, between(lat, 60, 90))
+high_lat_m <- rbind(high_lat_m, filter(megabiomes, between(lat, -90, -60)))
 
 mid_lat <- filter(biomes, between(lat, 30, 60))
 mid_lat <- rbind(mid_lat, filter(biomes, between(lat, -60, -30)))
+mid_lat_m <- filter(megabiomes, between(lat, 30, 60))
+mid_lat_m <- rbind(mid_lat_m, filter(megabiomes, between(lat, -60, -30)))
 
 low_lat <- filter(biomes, between(lat, -30, 30))
+low_lat_m <- filter(megabiomes, between(lat, -30, 30))
 
 #Count the biome changes between adjacent time slices
 changes_high <- c()
 changes_mid <- c()
 changes_low <- c()
+mega_high <- c()
+mega_mid <- c()
+mega_low <- c()
 
 for (j in 1:(length(slices)-1)){
-  changes_high[j] <- length(which(high_lat[,j] != high_lat[,j+1]))
-  changes_mid[j] <- length(which(mid_lat[,j] != mid_lat[,j+1]))
-  changes_low[j] <- length(which(low_lat[,j] != low_lat[,j+1]))
+  changes_high[j] <- length(which(high_lat[,j+2] != high_lat[,j+3]))
+  changes_mid[j] <- length(which(mid_lat[,j+2] != mid_lat[,j+3]))
+  changes_low[j] <- length(which(low_lat[,j+2] != low_lat[,j+3]))
+  mega_high[j] <- length(which(high_lat_m[,j+2] != high_lat_m[,j+3]))
+  mega_mid[j] <- length(which(mid_lat_m[,j+2] != mid_lat_m[,j+3]))
+  mega_low[j] <- length(which(low_lat_m[,j+2] != low_lat_m[,j+3]))
 }
 
 #Add slice midpoints to plot against
@@ -56,8 +76,17 @@ lat_plot <- as.data.frame(cbind(midpoints, changes_high, changes_mid, changes_lo
 lat_plot <- pivot_longer(lat_plot, !midpoints, names_to = "latitude",
                          names_prefix = "changes_")
 
+lat_plot_m <- as.data.frame(cbind(midpoints, mega_high, mega_mid, mega_low))
+lat_plot_m <- pivot_longer(lat_plot_m, !midpoints, names_to = "latitude",
+                         names_prefix = "mega_")
+
 #Plot
 ggplot(data = lat_plot, aes(x = midpoints, y = value, group = latitude, colour = latitude)) +
   geom_line() +
   xlab("Year") + ylab("Number of grid cell biome transitions") +
+  theme_classic()
+
+ggplot(data = lat_plot_m, aes(x = midpoints, y = value, group = latitude, colour = latitude)) +
+  geom_line() +
+  xlab("Year") + ylab("Number of grid cell megabiome transitions") +
   theme_classic()
