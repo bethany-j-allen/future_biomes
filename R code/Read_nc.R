ecec8880d7716fbae95b3538681a2d88a5a65d6c
Bethany Allen <allenb@bs-mbpr540.d.ethz.ch>
2022-12-21 13:49:47 +0100

    Rotate longitude values

--- R code/Read_nc.R
+++ R code/Read_nc.R
@@ -12,7 +12,7 @@ slices <- c("2000-2019", "2020-2039", "2040-2059", "2060-2079", "2080-2099",
 
 for (i in 1:length(slices)){
   #Create filename
-  filename <- paste0("data/netCDFs/xoazm_", slices[i], "AD_biome4out.nc")
+  filename <- paste0("data/netCDFs/RCP6/xoazm_", slices[i], "AD_biome4out.nc")
 
   #Read in netCDF
   netCDF <- nc_open(filename)
@@ -25,7 +25,7 @@ for (i in 1:length(slices)){
     lonlat <- as.matrix(expand.grid(lon,lat))
     biome_table <- data.frame(lonlat)
 
-    one_raster <- raster("data/netCDFs/xoazm_2000-2019AD_biome4out.nc")
+    one_raster <- raster("data/netCDFs/RCP6/xoazm_2000-2019AD_biome4out.nc")
     proj4string(one_raster) <- CRS("+proj=longlat")
     cell_area <- area(one_raster)
     biome_table <- cbind(biome_table, cell_area@data@values)
@@ -41,8 +41,15 @@ for (i in 1:length(slices)){
   colnames(biome_table)[(i + 3)] <- slices[i]
 }
 
-#Write table, removing NA values
+#Remove NA values
 biome_table <- na.omit(biome_table)
+
+#Swivel round longitude values
+for (j in 1:nrow(biome_table)) {
+  if (biome_table[j,1] > 180) {biome_table[j,1] <- biome_table[j,1] - 360}
+}
+
+#Save table
 write.csv(biome_table, "data/cleaned/RCP6.csv", row.names = F)
 
 #Read in biome conversion
