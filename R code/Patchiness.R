ca10d1776c8b6deef88fe29899d93acd52f2e3a4
Bethany Allen <bethany.allen@bsse.ethz.ch>
2023-04-15 09:56:02 +0200

    Add facets

--- R code/Patchiness.R
+++ R code/Patchiness.R
@@ -16,40 +16,49 @@ slices <- c("2000-2019", "2020-2039", "2040-2059", "2060-2079", "2080-2099",
             "2400-2419", "2420-2439", "2440-2459", "2460-2479", "2480-2499")
 midpoints <- seq(from = 2010, to = 2490, by = 20)
 megabiome_tags <- c("A", "B", "C", "D", "E", "F", "G", "H", "I")
+RCPs <- c("RCP2.6/xoazl", "RCP4.5/xoazk", "RCP6/xoazm")
 
-patch_table <- data.frame()
+to_plot <- data.frame()
 
-for (i in 1:length(slices)){
-  #Create filename
-  filename <- paste0("data/netCDFs/RCP6/xoazm_", slices[i], "AD_biome4out.nc")
+for (k in 1:length(RCPs)){
+  patch_table <- data.frame()
+  for (i in 1:length(slices)){
+    
+    #Create filename
+    filename <- paste0("data/netCDFs/", RCPs[k], "_", slices[i], "AD_biome4out.nc")
   
-  #Read in netCDF
-  biomes <- raster(filename, varname = "biome")
+    #Read in netCDF
+    biomes <- raster(filename, varname = "biome")
   
-  #Delineate patches
-  patches <- get_patches(biomes, directions = 8)
-  patches <- patches[[1]]
-  names(patches) <- gsub("class_", "", names(patches))
+    #Delineate patches
+    patches <- get_patches(biomes, directions = 8)
+    patches <- patches[[1]]
+    names(patches) <- gsub("class_", "", names(patches))
   
-  for (j in 1:28){
-    if (j %in% names(patches)){
-      patch_count <- length(raster::unique(patches[[paste0(j)]]))
-      patch_table[j,i] <- patch_count } else
-        { patch_table[j,i] <- 0 }
+    for (j in 1:28){
+      if (j %in% names(patches)){
+        patch_count <- length(raster::unique(patches[[paste0(j)]]))
+        patch_table[j,i] <- patch_count } else
+          { patch_table[j,i] <- 0 }
+    }
   }
+
+  #Clean table
+  colnames(patch_table) <- midpoints
+  patch_table$biomes <- seq(1, 28, 1)
+  #patch_table$labels <- conversion$V2
+  patch_table <- pivot_longer(patch_table, !biomes)
+  patch_table$RCP <- RCPs[k]
+  to_plot <- rbind(to_plot, patch_table)
 }
 
-#Clean table
-colnames(patch_table) <- midpoints
-patch_table$biomes <- seq(1, 28, 1)
-#patch_table$labels <- conversion$V2
-patch_table <- pivot_longer(patch_table, !biomes)
-
-patch_table$biomes <- as.factor(patch_table$biomes)
+to_plot$RCP <- gsub('/.*', '', to_plot$RCP)
+to_plot$biomes <- as.factor(to_plot$biomes)
 
 #Plot
-ggplot(data = patch_table, aes(x = name, y = value,
+ggplot(data = to_plot, aes(x = name, y = value,
                                 group = biomes, col = biomes)) +
   geom_line() +
+  facet_wrap( ~ RCP, ncol = 3) +
   xlab("Year") + ylab("Number of patches") +
   theme_classic()
