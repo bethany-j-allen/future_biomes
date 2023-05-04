f27d27674121b4e1ec281aa5700ff2b16c701f21
Bethany Allen <bethany.allen@bsse.ethz.ch>
2023-05-04 17:09:13 +0200

    Add overlap comparison with present day

--- R code/Biome_overlap.R
+++ R code/Biome_overlap.R
@@ -28,6 +28,7 @@ megabiome_tags <- c("A", "B", "C", "D", "E", "F", "G", "H", "I")
 conversion <- read.table("data/biome_conversion.txt", sep = ",")
 
 overlap_area_all <- data.frame(); overlap_area_m_all <- data.frame()
+overlap_area_p_all <- data.frame(); overlap_area_m_p_all <- data.frame()
 
 for (k in 1:length(filenames)){
   #Read in biome IDs
@@ -38,8 +39,9 @@ for (k in 1:length(filenames)){
   biomes <- na.omit(biomes)
   megabiomes <- na.omit(megabiomes)
 
-  #Count the biome changes between adjacent time slices
+  #Count the biome changes between adjacent time slices, and from present
   overlap_area <- data.frame(); overlap_area_m <- data.frame()
+  overlap_area_p <- data.frame(); overlap_area_m_p <- data.frame()
   
   for (j in 1:length(biome_tags)) {
     for (i in 1:(length(slices) - 1)) {
@@ -47,18 +49,26 @@ for (k in 1:length(filenames)){
       biome_area <- sum(biome_cells$area_km2)
       biome_overlap <- biome_cells[which(biome_cells[,i+3] == j),]
       overlap_area[j,i] <- (sum(biome_overlap$area_km2))/biome_area
+      
+      biome_overlap_p <- biome_cells[which(biome_cells[,4] == j),]
+      overlap_area_p[j,i] <- (sum(biome_overlap_p$area_km2))/biome_area
     
       if(j < 10){
         megabiome_cells <- megabiomes[which(megabiomes[,i+4] == megabiome_tags[j]),]
         megabiome_area <- sum(megabiome_cells$area_km2)
         megabiome_overlap <- megabiome_cells[which(megabiome_cells[,i+3] == megabiome_tags[j]),]
         overlap_area_m[j,i] <- (sum(megabiome_overlap$area_km2))/megabiome_area
+        
+        megabiome_overlap_p <- megabiome_cells[which(megabiome_cells[,4] == megabiome_tags[j]),]
+        overlap_area_m_p[j,i] <- (sum(megabiome_overlap_p$area_km2))/megabiome_area
       }
     }
   }
   #Add labels
   colnames(overlap_area) <- midpoints
   colnames(overlap_area_m) <- midpoints
+  colnames(overlap_area_p) <- midpoints
+  colnames(overlap_area_m_p) <- midpoints
   
   overlap_area$biome <- biome_tags
   overlap_area$file <- filenames[k]
@@ -66,21 +76,39 @@ for (k in 1:length(filenames)){
   overlap_area_m$megabiome <- megabiome_tags
   overlap_area_m$file <- mega_filenames[k]
   
+  overlap_area_p$biome <- biome_tags
+  overlap_area_p$file <- filenames[k]
+  
+  overlap_area_m_p$megabiome <- megabiome_tags
+  overlap_area_m_p$file <- mega_filenames[k]
+  
   overlap_area_all <- rbind(overlap_area_all, overlap_area)
   overlap_area_m_all <- rbind(overlap_area_m_all, overlap_area_m)
+  overlap_area_p_all <- rbind(overlap_area_p_all, overlap_area_p)
+  overlap_area_m_p_all <- rbind(overlap_area_m_p_all, overlap_area_m_p)
+  
+  #Track progress
+  print(k)
 }
 
 #Rotate data frames
 overlap_area_all <- pivot_longer(overlap_area_all, !c(biome, file))
 overlap_area_m_all <- pivot_longer(overlap_area_m_all, !c(megabiome, file))
+overlap_area_p_all <- pivot_longer(overlap_area_p_all, !c(biome, file))
+overlap_area_m_p_all <- pivot_longer(overlap_area_m_p_all, !c(megabiome, file))
 
 #Convert biome numbers to labels
 overlap_area_all <- left_join(overlap_area_all, conversion, by = c("biome" = "V1"))
-conversion <- distinct(conversion, V3, .keep_all = T)
-overlap_area_m_all <- left_join(overlap_area_m_all, conversion, by = c("megabiome" = "V3"))
+conversion_mega <- distinct(conversion, V3, .keep_all = T)
+overlap_area_m_all <- left_join(overlap_area_m_all, conversion_mega, by = c("megabiome" = "V3"))
+
+overlap_area_p_all <- left_join(overlap_area_p_all, conversion, by = c("biome" = "V1"))
+overlap_area_m_p_all <- left_join(overlap_area_m_p_all, conversion_mega, by = c("megabiome" = "V3"))
 
 overlap_area_all$name <- as.numeric(overlap_area_all$name)
 overlap_area_m_all$name <- as.numeric(overlap_area_m_all$name)
+overlap_area_p_all$name <- as.numeric(overlap_area_p_all$name)
+overlap_area_m_p_all$name <- as.numeric(overlap_area_m_p_all$name)
 
 #Plot results
 ggplot(data = overlap_area_all, aes(x = name, y = value, group = file, colour = file)) +
@@ -92,7 +120,7 @@ ggplot(data = overlap_area_all, aes(x = name, y = value, group = file, colour = 
   xlab("Year") + ylab("Proportion of biome area overlapping with previous slice") +
   scale_x_continuous(guide = guide_axis(angle = 90)) +
   theme_classic()
-ggsave("figures/Biome overlap.pdf", width = 10, height = 6, dpi = 600)
+ggsave("figures/Biome overlap previous.pdf", width = 10, height = 6, dpi = 600)
 
 ggplot(data = overlap_area_m_all, aes(x = name, y = value, group = file, colour = file)) +
   geom_line() +
@@ -103,4 +131,26 @@ ggplot(data = overlap_area_m_all, aes(x = name, y = value, group = file, colour 
   xlab("Year") + ylab("Proportion of megabiome area overlapping with previous slice") +
   scale_x_continuous(guide = guide_axis(angle = 90)) +
   theme_classic()
-ggsave("figures/Megabiome overlap.pdf", width = 10, height = 6, dpi = 600)
+ggsave("figures/Megabiome overlap previous.pdf", width = 10, height = 6, dpi = 600)
+
+ggplot(data = overlap_area_p_all, aes(x = name, y = value, group = file, colour = file)) +
+  geom_line() +
+  facet_wrap( ~ V2, ncol = 7, labeller = label_wrap_gen(multi_line = T)) +
+  scale_colour_manual(values = c("firebrick1", "firebrick4", "firebrick3",
+                                 "cadetblue1", "cadetblue4", "cadetblue3", 
+                                 "green2", "green4", "green3")) +
+  xlab("Year") + ylab("Proportion of biome area overlapping with present day") +
+  scale_x_continuous(guide = guide_axis(angle = 90)) +
+  theme_classic()
+ggsave("figures/Biome overlap present.pdf", width = 10, height = 6, dpi = 600)
+
+ggplot(data = overlap_area_m_p_all, aes(x = name, y = value, group = file, colour = file)) +
+  geom_line() +
+  facet_wrap( ~ V4, ncol = 3) +
+  scale_colour_manual(values = c("firebrick1", "firebrick4", "firebrick3",
+                                 "cadetblue1", "cadetblue4", "cadetblue3", 
+                                 "green2", "green4", "green3")) +
+  xlab("Year") + ylab("Proportion of megabiome area overlapping with present day") +
+  scale_x_continuous(guide = guide_axis(angle = 90)) +
+  theme_classic()
+ggsave("figures/Megabiome overlap present.pdf", width = 10, height = 6, dpi = 600)
